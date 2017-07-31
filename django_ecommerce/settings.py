4dbe5a429ee3844d6a2825dc865aa5ecd13c1643
Htet Naing Aung <justin.aung19@gmail.com>
2017-08-01 00:21:23 +0700

    upgrade to postgres db

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'django_ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'djangousr',
+        'PASSWORD': 'djangousr',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
