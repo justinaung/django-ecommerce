a2019b125e456528680da6544d47333ce032a99a
Justin <justin.aung19@gmail.com>
2017-09-24 02:00:12 +0700

    Set permissions and add test for API

--- django_ecommerce/main/api/viewsets.py
+++ django_ecommerce/main/api/viewsets.py
@@ -1,5 +1,7 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from django_ecommerce.main.api.permissions import IsOwnerOrReadOnly
 from django_ecommerce.main.api.serializers import StatusReportSerializer
 from django_ecommerce.main.models import StatusReport
 
@@ -7,3 +9,4 @@ from django_ecommerce.main.models import StatusReport
 class StatusReportViewSet(ModelViewSet):
     queryset = StatusReport.objects.all()
     serializer_class = StatusReportSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -25,7 +25,9 @@ from django_ecommerce.main.api.viewsets import StatusReportViewSet
 admin.autodiscover()
 
 router = routers.SimpleRouter()
-router.register(r'status_reports', StatusReportViewSet)
+router.register(
+    r'status_reports', StatusReportViewSet, base_name='status-reports'
+)
 
 urlpatterns = [
     url(r'^', include('django_ecommerce.main.urls', namespace='main')),
@@ -33,7 +35,7 @@ urlpatterns = [
     url(r'^api/v1/', include(router.urls, namespace='api')),
     url(r'^contact/', contact_views.contact, name='contact'),
     url(r'^edit/', payment_views.edit, name='edit'),
-    url(r'pages/', include('django.contrib.flatpages.urls')),
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^register/$', payment_views.register, name='register'),
     url(r'^sign_in/$', payment_views.sign_in, name='sign_in'),
     url(r'^sign_out/$', payment_views.sign_out, name='sign_out'),
