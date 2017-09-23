3c9be7be25f234fd74424c1cd19e08aa01916283
Justin <justin.aung19@gmail.com>
2017-09-23 19:19:44 +0700

    Modify member’s only page
    - add status form, status lists, and profile badge
    - add a template tag to handle gravatar

--- django_ecommerce/main/urls.py
+++ django_ecommerce/main/urls.py
@@ -4,4 +4,5 @@ from django_ecommerce.main import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^report$', views.report, name='report'),
 ]
--- django_ecommerce/main/views.py
+++ django_ecommerce/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from django_ecommerce.main.models import StatusReport
 from django_ecommerce.payments.models import User
 
 
@@ -8,6 +9,21 @@ def index(request):
     if uid is None:
         return render(request, 'main/home.html')
     else:
+        status = StatusReport.objects.all().order_by('-when')[:20]
         return render(
-            request, 'main/user.html', {'logged_in_user': User.get_by_id(uid)}
+            request,
+            'main/user.html',
+            {'logged_in_user': User.get_by_id(uid), 'reports': status},
         )
+
+
+def report(request):
+    if request.method == 'POST':
+        status = request.POST.get('status', '')
+
+        if status:
+            uid = request.session.get('user')
+            user = User.get_by_id(uid)
+            StatusReport.objects.create(user=user, status=status)
+
+        return index(request)
